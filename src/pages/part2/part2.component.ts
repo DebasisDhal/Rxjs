402da4f2e5232378acd979d0f74e1fc1e9276805
DebasisDhal <debasisdhal585@gmail.com>
2025-08-27 21:44:41 +0530

    rxjs operator part2

--- src/pages/part2/part2.component.ts
+++ src/pages/part2/part2.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { filter, from, map, of } from 'rxjs';
+import { filter, from, interval, map, of, take } from 'rxjs';
 import { ServService } from './serv.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-part2',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './part2.component.html',
   styleUrl: './part2.component.css'
 })
@@ -14,6 +15,8 @@ export class Part2Component {
   noList$ =from([1,2,3,4,5,6,7,8,9,10]);
   rolNoList$ =of([101,102,103,104,105,106,107,108,109,110]);
   masterService =inject(ServService);
+  searchTerm = new FormControl();
+  timeInterval$ = interval(1000);
 
   constructor() { 
     this.noList$.subscribe(x=>{ //one by one value
@@ -33,7 +36,24 @@ export class Part2Component {
       console.log("getUser",res);
       
     })
+    this.masterService.getSingleUser().subscribe((res:any)=>{
+      console.log("getSingleUser",res);
+      
+    })
+
+    this.searchTerm.valueChanges.pipe(
+      filter(searchText=> searchText.length >=3)).subscribe((res:any)=>{ // whenever type in input box it will give the value in after 3 character
+      console.log("searchTerm",res);
+      
+    });
+
+    this.timeInterval$.pipe(
+      take(10) // it will give the value from 0 to 9 after that it will stop
+    ).subscribe((res:any)=>{
+      console.log("timeInterval",res);
+    })
   }
+
 
 
 }
--- src/pages/part2/serv.service.ts
+++ src/pages/part2/serv.service.ts
@@ -1,6 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,18 @@ export class ServService {
   constructor(private http:HttpClient) { }
 
   getUserS(){
-   return this.http.get("https://jsonplaceholder.typicode.com/users");
+   return this.http.get<any[]>("https://jsonplaceholder.typicode.com/users").pipe(
+    tap(userList=>
+      console.log("userLists",userList)), // all the data store in userList
+   
+    map((userList:any[])=>userList.map((user:any)=>{ // map return array of object id, name
+      return { id: user.id, name: user.name };
+    }))
+   );
+  };
+  getSingleUser(){
+    return this.http.get<any>("https://jsonplaceholder.typicode.com/users/1").pipe( // get single user data of address
+      map((userData:any)=> userData.address)
+    );
   }
 }
