050935c20566efcf5ef849881b770b7119ad8005
DebasisDhal <debasisdhal585@gmail.com>
2025-09-05 22:11:58 +0530

    done part4 concatMap|exhaustMap

--- src/pages/par4/par4.component.ts
+++ src/pages/par4/par4.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { forkJoin, of, switchMap } from 'rxjs';
+import { concatMap, exhaustMap, forkJoin, of, Subject, switchMap } from 'rxjs';
 import { MasterService } from '../master.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
@@ -14,6 +14,7 @@ export class Par4Component {
 
   master = inject(MasterService);
   searchcontrol =new FormControl('');
+  loginData$ = new Subject<void>();
 
   constructor(){
   var  stateData$ = of(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']);
@@ -45,7 +46,7 @@ export class Par4Component {
     console.log(result  ,"forkjoin users and posts");
   });
   // this.searDAta();
-  this.searchWithMapData();
+  // this.searchWithMapData();
   }
   
   // here multiple api call for each character
@@ -61,13 +62,39 @@ export class Par4Component {
 // }
 
   // here single api call for each character Help of switchmap
-searchWithMapData() {
-  this.searchcontrol.valueChanges.pipe(
-    switchMap((search: string | null) => this.master.getSearchData(search ?? ''))
-  ).subscribe((res: any) => {
-    console.log(res);
+// searchWithMapData() {
+//   this.searchcontrol.valueChanges.pipe(
+//     switchMap((search: string | null) => this.master.getSearchData(search ?? ''))
+//   ).subscribe((res: any) => {
+//     console.log(res);
+//   });
+// }
+
+// here  api call for each character Help of concatmap one by one sequentially call
+// searchWithMapData() { 
+//   this.searchcontrol.valueChanges.pipe(
+//     concatMap((search: string | null) => this.master.getSearchData(search ?? ''))
+//   ).subscribe((res: any) => {
+//     console.log(res);
+//   });
+// }
+
+onLogin(){
+  this.master.gertusers().subscribe((res:any)=>{
+    console.log(res,"users data");
   });
 }
-
+// onLogin(){
+//   this.loginData$.pipe(
+//     exhaustMap(() => this.master.gertusers())
+//   ).subscribe((res: any) => {
+//     console.log(res, "users data");
+//   });
+//   this.master.gertusers().subscribe((res:any)=>{
+//     console.log(res,"users data");
+//   });
+// }
 
 }
+
+
